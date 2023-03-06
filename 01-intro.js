8b19b11a0e017024daa7bbf74fee55db9bd3d42c
ezhivas <akvatonk@gmil.com>
2023-03-06 19:04:41 +0200

    first commit

---
+++ 01-intro.js
@@ -0,0 +1,14 @@
+// __dirname
+
+// __filename
+
+// require
+
+// module
+
+// process
+
+console.log(__dirname);
+setInterval(() => {
+  console.log("Hello world");
+}, 1000);
---
+++ 03-modules.js
@@ -0,0 +1,8 @@
+const names = require("./04-names");
+const sayHi = require("./05-utils");
+const data = require("./06-alternative-flavor");
+sayHi("susan");
+sayHi(names.john);
+sayHi(names.peter);
+
+const values = require("./07-mind-grenade");
